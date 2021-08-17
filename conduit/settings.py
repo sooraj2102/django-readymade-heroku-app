9db9f949800419ee9de8b9555393152a866e27b5
Sooraj Shingari <srjshingari@gmail.com>
2021-08-17 21:23:36 +0530

    Update settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
